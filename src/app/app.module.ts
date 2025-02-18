1a80a6e6abb7f19fd79c4791bdb0598217319b3a
JasserBA <jasserbenabdallah6@gmail.com>
2025-02-18 10:22:01 +0100

    navbar-added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from "@angular/common/http"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
+import { AddEmployeeComponent } from './pages/add-employee/add-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    AddEmployeeComponent
   ],
   imports: [
     BrowserModule,
